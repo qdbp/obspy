75c138651e638924d3645494e5b01c1e05401478
Tobias Megies <megies@geophysik.uni-muenchen.de>
2020-12-22 10:47:12 +0100

    hide matplotlib.cm import inside functions

--- obspy/imaging/source.py
+++ obspy/imaging/source.py
@@ -14,7 +14,6 @@ Functions to compute and plot radiation patterns
 """
 
 import numpy as np
-from matplotlib.cm import get_cmap
 
 from obspy.core.event.source import farfield
 from obspy.imaging.scripts.mopad import MomentTensor, BeachBall
@@ -186,6 +185,7 @@ def _plot_radiation_pattern_sphere(
     :param type: 'P' or 'S' (P or S wave).
     """
     import matplotlib.pyplot as plt
+    from matplotlib.cm import get_cmap
     type = type.upper()
     if type not in ("P", "S"):
         msg = ("type must be 'P' or 'S'")
@@ -267,6 +267,7 @@ def _plot_radiation_pattern_quiver(ax3d, ned_mt, type):
     :param type: 'P' or 'S' (P or S wave).
     """
     import matplotlib.pyplot as plt
+    from matplotlib.cm import get_cmap
 
     type = type.upper()
     if type not in ("P", "S"):
@@ -325,6 +326,7 @@ def _plot_beachball(ax2d, rtp_mt):
     :param rtp_mt: moment tensor in RTP convention
     """
     import matplotlib.pyplot as plt
+    from matplotlib.cm import get_cmap
     norm = plt.Normalize(-1., 1.)
     cmap = get_cmap('bwr')
     bball = beach(rtp_mt, xy=(0, 0), width=50, facecolor=cmap(norm(0.7)),
