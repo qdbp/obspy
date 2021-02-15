f68d2a0301d2ef065993274a8e3ba50c3695425f
Damian Kula <dkula@unistra.fr>
2021-02-15 14:10:54 +0100

    converts second plot to cartopy
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- misc/docs/source/tutorial/code_snippets/basemap_plot_with_beachballs2.py
+++ misc/docs/source/tutorial/code_snippets/basemap_plot_with_beachballs2.py
@@ -1,24 +1,24 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import cartopy
 
 from obspy.imaging.beachball import beach
 
 
-m = Basemap(projection='cyl', lon_0=142.36929, lat_0=38.3215,
-            resolution='c')
+projection = cartopy.crs.PlateCarree(central_longitude=142.0)
 
-m.drawcoastlines()
-m.fillcontinents()
-m.drawparallels(np.arange(-90., 120., 30.))
-m.drawmeridians(np.arange(0., 420., 60.))
-m.drawmapboundary()
+fig = plt.figure(dpi=150)
+ax = fig.add_subplot(111, projection=projection)
+ax.set_extent((-180, 180, -90, 90))
+ax.coastlines()
+ax.gridlines()
 
-x, y = m(142.36929, 38.3215)
+x, y = projection.transform_point(x=142.36929, y=38.3215,
+                                  src_crs=cartopy.crs.Geodetic())
 focmecs = [0.136, -0.591, 0.455, -0.396, 0.046, -0.615]
 
 ax = plt.gca()
 b = beach(focmecs, xy=(x, y), width=10, linewidth=1, alpha=0.85)
 b.set_zorder(10)
 ax.add_collection(b)
+
 plt.show()
