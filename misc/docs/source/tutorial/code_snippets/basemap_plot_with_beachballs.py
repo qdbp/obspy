ef731eed033a7170c77c6ffcdd3cc2a55070a289
Damian Kula <dkula@unistra.fr>
2021-02-15 15:25:25 +0100

    converts third plot to cartopy
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- misc/docs/source/tutorial/code_snippets/basemap_plot_with_beachballs.py
+++ misc/docs/source/tutorial/code_snippets/basemap_plot_with_beachballs.py
@@ -1,8 +1,10 @@
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import gzip
-
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import matplotlib.ticker as mticker
+import numpy as np
 
 from obspy.imaging.beachball import beach
 
@@ -17,44 +19,54 @@ with gzip.open("srtm_1240-1300E_4740-4750N.asc.gz") as fp:
 lats = np.linspace(47.8333, 47.6666, srtm.shape[0])
 lons = np.linspace(12.6666, 13.0000, srtm.shape[1])
 
-# create Basemap instance with Mercator projection
-# we want a slightly smaller region than covered by our SRTM data
-m = Basemap(projection='merc', lon_0=13, lat_0=48, resolution="h",
-            llcrnrlon=12.75, llcrnrlat=47.69, urcrnrlon=12.95, urcrnrlat=47.81)
+# Prepare figure and Axis object with a proper projection and extent
+projection = ccrs.PlateCarree()
+fig = plt.figure(dpi=150)
+ax = fig.add_subplot(111, projection=projection)
+ax.set_extent((12.75, 12.95, 47.69, 47.81))
 
 # create grids and compute map projection coordinates for lon/lat grid
-x, y = m(*np.meshgrid(lons, lats))
+grid = projection.transform_points(ccrs.Geodetic(),
+                                   *np.meshgrid(lons, lats),  # unpacked x, y
+                                   srtm)  # z from topo data
 
 # Make contour plot
-cs = m.contour(x, y, srtm, 40, colors="k", lw=0.5, alpha=0.3)
-m.drawcountries(color="red", linewidth=1)
+ax.contour(*grid.T, transform=projection, levels=30)
 
-# Draw a lon/lat grid (20 lines for an interval of one degree)
-m.drawparallels(np.linspace(47, 48, 21), labels=[1, 1, 0, 0], fmt="%.2f",
-                dashes=[2, 2])
-m.drawmeridians(np.linspace(12, 13, 21), labels=[0, 0, 1, 1], fmt="%.2f",
-                dashes=[2, 2])
+# Draw country borders with red
+ax.add_feature(cfeature.BORDERS.with_scale('50m'), edgecolor='red')
+
+# Draw a lon/lat grid and format the labels
+gl = ax.gridlines(crs=projection, draw_labels=True)
+
+gl.xlocator = mticker.FixedLocator([12.75, 12.8, 12.85, 12.9, 12.95])
+gl.ylocator = mticker.FixedLocator([47.7, 47.75, 47.8])
+gl.xformatter = LongitudeFormatter()
+gl.yformatter = LatitudeFormatter()
+gl.ylabel_style = {'size': 13, 'color': 'gray'}
+gl.xlabel_style = {'weight': 'bold'}
+
 
 # Plot station positions and names into the map
 # again we have to compute the projection of our lon/lat values
-lats = [47.761659, 47.7405, 47.755100, 47.737167]
-lons = [12.864466, 12.8671, 12.849660, 12.795714]
+lats = np.array([47.761659, 47.7405, 47.755100, 47.737167])
+lons = np.array([12.864466, 12.8671, 12.849660, 12.795714])
 names = [" RMOA", " RNON", " RTSH", " RJOB"]
-x, y = m(lons, lats)
-m.scatter(x, y, 200, color="r", marker="v", edgecolor="k", zorder=3)
+x, y, _ = projection.transform_points(ccrs.Geodetic(), lons, lats).T
+ax.scatter(x, y, 200, color="r", marker="v", edgecolor="k", zorder=3)
 for i in range(len(names)):
     plt.text(x[i], y[i], names[i], va="top", family="monospace", weight="bold")
 
 # Add beachballs for two events
-lats = [47.751602, 47.75577]
-lons = [12.866492, 12.893850]
-x, y = m(lons, lats)
+lats = np.array([47.751602, 47.75577])
+lons = np.array([12.866492, 12.893850])
+points = projection.transform_points(ccrs.Geodetic(), lons, lats)
+
 # Two focal mechanisms for beachball routine, specified as [strike, dip, rake]
 focmecs = [[80, 50, 80], [85, 30, 90]]
-ax = plt.gca()
 for i in range(len(focmecs)):
-    b = beach(focmecs[i], xy=(x[i], y[i]), width=1000, linewidth=1)
-    b.set_zorder(10)
+    b = beach(focmecs[i], xy=(points[i][0], points[i][1]),
+              width=0.008, linewidth=1, zorder=10)
     ax.add_collection(b)
 
 plt.show()
