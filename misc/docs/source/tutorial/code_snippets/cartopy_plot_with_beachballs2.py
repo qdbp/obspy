e48d2604fb99d702db76050811f85dca47e88873
Damian Kula <dkula@unistra.fr>
2021-02-15 16:06:31 +0100

    Renames files and uses cartopy instead of basemap in docs
    Signed-off-by: Damian Kula <dkula@unistra.fr>

--- misc/docs/source/tutorial/code_snippets/cartopy_plot_with_beachballs2.py
+++ misc/docs/source/tutorial/code_snippets/cartopy_plot_with_beachballs2.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-import cartopy
+import cartopy.crs as ccrs
 
 from obspy.imaging.beachball import beach
 
 
-projection = cartopy.crs.PlateCarree(central_longitude=142.0)
+projection = ccrs.PlateCarree(central_longitude=142.0)
 
 fig = plt.figure(dpi=150)
 ax = fig.add_subplot(111, projection=projection)
@@ -13,7 +13,7 @@ ax.coastlines()
 ax.gridlines()
 
 x, y = projection.transform_point(x=142.36929, y=38.3215,
-                                  src_crs=cartopy.crs.Geodetic())
+                                  src_crs=ccrs.Geodetic())
 focmecs = [0.136, -0.591, 0.455, -0.396, 0.046, -0.615]
 
 ax = plt.gca()
